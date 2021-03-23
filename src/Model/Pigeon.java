1be9711983347f7422fb920b29fc0372a906f024
Valentin Roche <35733063+valentin-roche@users.noreply.github.com>
2021-03-23 17:47:36 -0400

    fin classes modèle de base

--- src/Model/Pigeon.java
+++ src/Model/Pigeon.java
@@ -3,21 +3,72 @@ package Model;
 import java.util.ArrayList;
 import java.util.Vector;
 
-public class Pigeon extends Thread{
+public class Pigeon extends Thread {
     private Vector<Integer> pos;
     private boolean isAwake;
     private Integer speed;
     private Seed target;
 
     public void iterate(ArrayList<Seed> currentSeeds) {
+        Seed targetSeed = findClosestSeed(currentSeeds);
+        if (targetSeed != null) {
+            setTarget(targetSeed);
+        }
 
+        //Check if closest seed is still target or if target is not set
+
+        // Check if seed is at eating distance
+        // if so eat
+
+        // Otherwise move
+
+            // Calculate seed direction
+
+            // Calculate movement vector
+
+            // Add movement to pos
     }
 
     private Seed findClosestSeed(ArrayList<Seed> currentSeeds) {
-        return null;
+        if (currentSeeds.size() == 0) {
+            return null;
+        }
+        else {
+            Seed closestSeed = null;
+            float shortestDistance = 200000;
+            for ( Seed seed : currentSeeds) {
+                float distance = calculateDistanceTo(seed);
+                if (distance < shortestDistance) {
+                    shortestDistance = distance;
+                    closestSeed = seed;
+                }
+            }
+
+            return closestSeed;
+        }
+    }
+
+    private float calculateDistanceTo(Seed seed) {
+        return 200000;
     }
 
     public String toSring() {
-        return ("Pigeon a la position " + pos);
+        return ("Pigeon a la position " + getPos());
+    }
+
+    public Vector<Integer> getPos() {
+        return pos;
+    }
+
+    public void setPos(Vector<Integer> pos) {
+        this.pos = pos;
+    }
+
+    public Seed getTarget() {
+        return target;
+    }
+
+    public void setTarget(Seed target) {
+        this.target = target;
     }
 }
